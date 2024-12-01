0b1d6365acce797ddb659a2e52f46e92c1a12f8e
Mike Brocchi <brocco@users.noreply.github.com>
2024-12-01 13:05:29 -0500

    day 1 solution

--- apps/cli/src/lib/solutions/2024/day-1/index.ts
+++ apps/cli/src/lib/solutions/2024/day-1/index.ts
@@ -1,3 +1,39 @@
-export default function (input: string): string {
-  return input.toUpperCase();
+export default function (part: 1 | 2, input: string): string {
+  const { lefts, rights } = input.split('\n').reduce(
+    (acc, line) => {
+      const [left, right] = line.split('   ');
+      acc.lefts.push(parseInt(left));
+      acc.rights.push(parseInt(right));
+      return acc;
+    },
+    { lefts: [], rights: [] } as { lefts: number[]; rights: number[] }
+  );
+  if (part === 1) {
+    return part1(lefts, rights).toString();
+  }
+  return part2(lefts, rights).toString();
+}
+
+function part1(lefts: number[], rights: number[]): number {
+  const sortedLefts = lefts.sort((a, b) => a - b);
+  const sortedRights = rights.sort((a, b) => a - b);
+  let diffSum = 0;
+  for (let i = 0; i < sortedLefts.length; i++) {
+    diffSum += Math.abs(sortedLefts[i] - sortedRights[i]);
+  }
+  return diffSum;
+}
+
+function part2(lefts: number[], rights: number[]): number {
+  const rightCountMap = rights.reduce((acc, right) => {
+    acc[right] = (acc[right] || 0) + 1;
+    return acc;
+  }, {} as Record<number, number>);
+
+  return lefts.reduce((acc, left) => {
+    if (rightCountMap[left]) {
+      acc += left * rightCountMap[left];
+    }
+    return acc;
+  }, 0);
 }
