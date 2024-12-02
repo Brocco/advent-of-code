9395b36512d2b25b24c3e42559b9ecf786cd509a
Mike Brocchi <brocco@users.noreply.github.com>
2024-12-02 15:37:29 -0500

    completion of day 2

--- apps/cli/src/lib/solutions/2024/day-2/a.ts
+++ apps/cli/src/lib/solutions/2024/day-2/a.ts
@@ -1,3 +1,23 @@
 export default function (input: string): string {
-  return '';
+  // const reports = input.split('\n');
+  // const levels = reports.map((report) => report.split(' '));
+
+  const safeCount = input
+    .split('\n')
+    .map((report) => {
+      const levels = report.split(' ').map((level) => parseInt(level));
+      const inc = levels[1] > levels[0];
+      let safe = true;
+      for (let i = 0; i < levels.length - 1; i++) {
+        const diff = levels[i + 1] - levels[i];
+        if ((inc && diff < 0) || (!inc && diff > 0) || Math.abs(diff) > 3 || diff === 0) {
+          safe = false;
+          break;
+        }
+      }
+      return safe;
+    })
+    .filter((safe) => safe).length;
+
+  return safeCount.toString();
 }
--- apps/cli/src/lib/solutions/2024/day-2/b.ts
+++ apps/cli/src/lib/solutions/2024/day-2/b.ts
@@ -1,3 +1,33 @@
 export default function (input: string): string {
-  return '';
+  // const reports = input.split('\n');
+  // const levels = reports.map((report) => report.split(' '));
+
+  const safeCount = input
+    .split('\n')
+    .map((report) => {
+      const levels = report.split(' ').map((level) => parseInt(level));
+      let unsafeCount = getUnsafeCount(levels);
+      let i = 0;
+      while (unsafeCount > 0 && i < levels.length) {
+        const subLevels = levels.slice(0, i).concat(levels.slice(i + 1));
+        unsafeCount = getUnsafeCount(subLevels);
+        i++;
+      }
+      return unsafeCount === 0;
+    })
+    .filter((safe) => safe).length;
+
+  return safeCount.toString();
+}
+
+function getUnsafeCount(levels: number[]): number {
+  const inc = levels[1] > levels[0];
+  let unsafeCount = 0;
+  for (let i = 0; i < levels.length - 1; i++) {
+    const diff = levels[i + 1] - levels[i];
+    if ((inc && diff < 0) || (!inc && diff > 0) || Math.abs(diff) > 3 || diff === 0) {
+      unsafeCount++;
+    }
+  }
+  return unsafeCount;
 }
