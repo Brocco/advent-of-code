7b14beea110742186ebede73f36d179c21e3fadb
Mike Brocchi <brocco@users.noreply.github.com>
2024-12-03 15:41:25 -0500

    completion of day 3

--- apps/cli/src/lib/solutions/2024/day-3/a.ts
+++ apps/cli/src/lib/solutions/2024/day-3/a.ts
@@ -1,3 +1,23 @@
 export default function (input: string): string {
-  return input;
+  const mulRegex = /mul\(\d{1,3},\d{1,3}\)/gm;
+  const mulMatches = input.matchAll(mulRegex);
+
+  if (mulMatches === null) {
+    return '0';
+  }
+
+  const muls = Array.from(mulMatches, (match) => match[0]);
+
+  const result = muls.reduce((acc, mul) => {
+    const [num1, num2] = mul
+      .replace('mul(', '')
+      .replace(')', '')
+      .split(',')
+      .map((n) => {
+        return parseInt(n, 10);
+      });
+    return acc + num1 * num2;
+  }, 0);
+
+  return result.toString();
 }
--- apps/cli/src/lib/solutions/2024/day-3/b.ts
+++ apps/cli/src/lib/solutions/2024/day-3/b.ts
@@ -1,3 +1,43 @@
 export default function (input: string): string {
-  return input;
+  const updatedInput = removeDonts(input);
+  const mulRegex = /mul\(\d{1,3},\d{1,3}\)/gm;
+  const mulMatches = updatedInput.matchAll(mulRegex);
+
+  if (mulMatches === null) {
+    return '0';
+  }
+
+  const muls = Array.from(mulMatches, (match) => match[0]);
+
+  const result = muls.reduce((acc, mul) => {
+    const [num1, num2] = mul
+      .replace('mul(', '')
+      .replace(')', '')
+      .split(',')
+      .map((n) => {
+        return parseInt(n, 10);
+      });
+    return acc + num1 * num2;
+  }, 0);
+
+  return result.toString();
+}
+
+function removeDonts(input: string): string {
+  const doFn = `do()`;
+  const dontFn = `don't()`;
+
+  let strippedInput = input;
+  let dontIndex = strippedInput.indexOf(dontFn);
+  while (dontIndex !== -1) {
+    const doIndex = strippedInput.indexOf(doFn, dontIndex);
+    if (doIndex === -1) {
+      strippedInput = strippedInput.substring(0, dontIndex);
+    } else {
+      strippedInput =
+        strippedInput.substring(0, dontIndex) + strippedInput.substring(doIndex + doFn.length);
+    }
+    dontIndex = strippedInput.indexOf(dontFn);
+  }
+  return strippedInput;
 }
