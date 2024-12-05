6d769a02c8465fb14cc3f93cde11b2493374d02f
Mike Brocchi <brocco@users.noreply.github.com>
2024-12-04 21:47:31 -0500

    completed day 4

--- apps/cli/src/lib/solutions/2024/day-4/a.ts
+++ apps/cli/src/lib/solutions/2024/day-4/a.ts
@@ -1,3 +1,51 @@
 export default function (input: string): string {
-  return input;
+  const wordToFind = 'XMAS';
+  const chars = input.split('\n').map((row) => row.split(''));
+
+  let wordCount = 0;
+  for (let y = 0; y < chars.length; y++) {
+    for (let x = 0; x < chars[0].length; x++) {
+      wordCount += getWordCount(x, y, chars, wordToFind);
+    }
+  }
+  return wordCount.toString();
+}
+
+function getWordCount(x: number, y: number, chars: string[][], word: string): number {
+  let wordFindCount = 0;
+
+  wordFindCount += findCount(x, 1, y, 0, chars, word);
+  wordFindCount += findCount(x, -1, y, 0, chars, word);
+  wordFindCount += findCount(x, 0, y, 1, chars, word);
+  wordFindCount += findCount(x, 0, y, -1, chars, word);
+  wordFindCount += findCount(x, 1, y, 1, chars, word);
+  wordFindCount += findCount(x, -1, y, -1, chars, word);
+  wordFindCount += findCount(x, 1, y, -1, chars, word);
+  wordFindCount += findCount(x, -1, y, 1, chars, word);
+
+  return wordFindCount;
+}
+
+function findCount(
+  x: number,
+  xDiff: number,
+  y: number,
+  yDiff: number,
+  chars: string[][],
+  word: string
+): number {
+  let letters = '';
+
+  for (let m = 0; m < word.length; m++) {
+    if (
+      y + m * yDiff >= 0 &&
+      y + m * yDiff < chars.length &&
+      x + m * xDiff >= 0 &&
+      x + m * xDiff < chars[0].length
+    ) {
+      letters += chars[y + m * yDiff][x + m * xDiff];
+    }
+  }
+
+  return letters == word ? 1 : 0;
 }
--- apps/cli/src/lib/solutions/2024/day-4/b.ts
+++ apps/cli/src/lib/solutions/2024/day-4/b.ts
@@ -1,3 +1,23 @@
 export default function (input: string): string {
-  return input;
+  const wordToFind = 'MAS';
+  const chars = input.split('\n').map((row) => row.split(''));
+
+  let wordCount = 0;
+  for (let y = 1; y < chars.length - 1; y++) {
+    for (let x = 1; x < chars[0].length - 1; x++) {
+      wordCount += getWordCount(x, y, chars, wordToFind);
+    }
+  }
+  return wordCount.toString();
+}
+
+function getWordCount(x: number, y: number, chars: string[][], word: string): number {
+  const tlbr = chars[y - 1][x - 1] + chars[y][x] + chars[y + 1][x + 1];
+  const bltr = chars[y + 1][x - 1] + chars[y][x] + chars[y - 1][x + 1];
+  const revWord = word.split('').reverse().join('');
+
+  if ((tlbr == word || tlbr == revWord) && (bltr == word || bltr == revWord)) {
+    return 1;
+  }
+  return 0;
 }
